6ee129713c8471b700c9fab319384e9995c6308a
Furkan İnce <89702333+bfurkanince@users.noreply.github.com>
2021-09-05 23:15:30 +0300

    Update BookController.java
    Respons'lar mask'lendi

--- src/main/java/com/bfi/springBootMongoDb/controller/BookController.java
+++ src/main/java/com/bfi/springBootMongoDb/controller/BookController.java
@@ -2,7 +2,11 @@ package com.bfi.springBootMongoDb.controller;
 
 import com.bfi.springBootMongoDb.entity.Book;
 import com.bfi.springBootMongoDb.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,29 +15,35 @@ import java.util.Optional;
 @RestController
 public class BookController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(BookController.class);
+
     @Autowired
     private BookRepository bookRepository;
 
     @PostMapping("/addBook")
-    public String saveBook(@RequestBody Book book){
+    public ResponseEntity<HttpStatus> saveBook(@RequestBody Book book){
         bookRepository.save(book);
-        return "Added book with id : " + book.getId();
+        LOG.info("Added book with id : " + book.getId());
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @GetMapping("/findAllBooks")
-    public List<Book> getBooks(){
-        return bookRepository.findAll();
+    public ResponseEntity<List<Book>> getBooks(){
+        List<Book> allBooksList = bookRepository.findAll() ;
+        return new ResponseEntity(allBooksList , HttpStatus.FOUND);
     }
 
     @GetMapping("/findAllBooks/{id}")
-    public Optional<Book> getBooks(@PathVariable String id){
-        return bookRepository.findById(id);
+    public ResponseEntity<Optional<Book>> getBookById(@PathVariable String id){
+        Optional<Book> book = bookRepository.findById(id);
+        return new ResponseEntity(book , HttpStatus.FOUND );
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteBook(@PathVariable String id ){
+    public ResponseEntity<HttpStatus> deleteBook(@PathVariable String id ){
         bookRepository.deleteById(id);
-        return "book deleted with id " + id;
+        LOG.info("book deleted with id " + id);
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
 }
